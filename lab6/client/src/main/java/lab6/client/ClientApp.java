1ba6252f74e5882f02a273f00075286972227e1a
marivsteo <mariusteo99@gmail.com>
2020-03-30 16:45:59 +0300

    Minor changes

--- lab6/client/src/main/java/lab6/client/ClientApp.java
+++ lab6/client/src/main/java/lab6/client/ClientApp.java
@@ -1,4 +1,7 @@
 package lab6.client;
 
 public class ClientApp {
+    public static void main(String[] args) {
+        System.out.println("Hello!!!");
+    }
 }
--- lab6/server/src/main/java/lab6/server/ServerApp.java
+++ lab6/server/src/main/java/lab6/server/ServerApp.java
@@ -1,4 +1,7 @@
 package lab6.server;
 
 public class ServerApp {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
 }
